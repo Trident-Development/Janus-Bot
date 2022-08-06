aab8c3b4120d5f69cca7993a79d88ba5b074f8ee
LuanTruong <truongluan303@gmail.com>
2022-08-05 22:36:46 -0700

    restructure

--- app.py
+++ app.py
@@ -5,13 +5,14 @@ from discord_slash import SlashCommand
 
 from config import DISCORD_TOKEN
 
-
 _LOGGER = logging.getLogger(__name__)
 
 bot = commands.Bot(command_prefix="/")
 slash = SlashCommand(bot, sync_commands=True, sync_on_cog_reload=True)
 
-bot.load_extension("cog")
-bot.run(DISCORD_TOKEN)
+bot.load_extension("disc.cog")
 
 _LOGGER.info("GeekBot started!")
+
+
+bot.run(DISCORD_TOKEN)
