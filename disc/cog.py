a2695720d1d405960ab0079d036d4cf3db9b1c96
Allen Tran <allentran08@gmail.com>
2022-08-06 08:47:30 -0500

    break up long line for content

--- disc/cog.py
+++ disc/cog.py
@@ -17,7 +17,12 @@ class Slash(commands.Cog):
     async def _post_job(self, ctx: SlashContext, job):
         linkedin = LinkedIn()
         data = linkedin.get_job_info(job)
-        await ctx.send(content=f"Check out this job on LinkedIn!\n\n>>> **{data.title}**\n\nLocation: {data.location}.\n```Job Details:\n\nThis job was posted {data.posted_time_ago}.\n{data.summary}\n\nApply Now: {data.url}``` {data.company_pic_url}")
+        message = f"Check out this job on LinkedIn!\n\n\
+            >>> **{data.title}**\n\nLocation: {data.location}.\n\
+            ```Job Details:\n\nThis job was posted {data.posted_time_ago}.\n\
+            {data.summary}\n\nApply Now: {data.url}``` {data.company_pic_url}"
+            
+        await ctx.send(content=message)
 
     @cog_ext.cog_slash(name="help", description=Descriptions.HELP)
     async def _help(self, ctx: SlashContext):
