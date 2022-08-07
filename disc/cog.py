16a321cb7916ef7e09e7ba7c45a2c84e101f6613
LuanTruong <truongluan303@gmail.com>
2022-08-06 17:50:19 -0700

    Revert "Adds view profile feature (#12)"
    This reverts commit 94231f375ac8f7e6f09707973536e47c52868120.

--- disc/cog.py
+++ disc/cog.py
@@ -8,11 +8,8 @@ from discord_slash import SlashContext
 from discord_slash.model import ButtonStyle
 from discord_slash.utils.manage_components import create_actionrow
 from discord_slash.utils.manage_components import create_button
-from linkedin_api import Linkedin as LinkedInProfileAPI
 
-from config import LINKEDIN_ACCOUNT
-from config import LINKEDIN_PASSWORD
-from job_boards_scrapers import LinkedIn as LinkedInJobAPI
+from job_boards_scrapers import LinkedIn
 from utils import Colors
 
 
@@ -27,63 +24,7 @@ class Descriptions:
 class Slash(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.linkedin_job_api = LinkedInJobAPI()
-        self.linkedin_profile_api = LinkedInProfileAPI(
-            LINKEDIN_ACCOUNT, LINKEDIN_PASSWORD
-        )
-
-    @cog_ext.cog_slash(name="view-profile", description=Descriptions.POST_JOB)
-    async def _view_profile(self, ctx: SlashContext, profile):
-
-        await ctx.send("Hang on, grabbing details for you...", hidden=True)
-
-        get_profile = self.linkedin_profile_api.get_profile(profile)
-
-        time_string = ""
-        for job in get_profile["experience"]:
-            time_period = job["timePeriod"]
-            if "endDate" in time_period:
-                time_string += (
-                    f"**🚀 {job['companyName']}** ({time_period['startDate']['month']},"
-                    f"{time_period['startDate']['year']} to {time_period['endDate']['month']}, "
-                    f"{time_period['endDate']['year']})\n*{job['title']}*\n\n"
-                )
-            else:
-                time_string += (
-                    f"**🚀 {job['companyName']}** ({time_period['startDate']['month']}, "
-                    f"{time_period['startDate']['year']} to Present)\n*{job['title']}*\n\n"
-                )
-
-        school_string = ""
-        for education in get_profile["education"]:
-            school_string += (
-                f"**🎖 {education['schoolName']}** - {education['fieldOfStudy']}\n\n"
-            )
-
-        description = (
-            f"**{get_profile['headline']}**\n\n"
-            f"Based in {get_profile['geoLocationName']}, {get_profile['location']['basicLocation']['countryCode'].upper()}, "
-            f"{get_profile['firstName']} is currently working in the **{get_profile['industryName']}** industry.\n\n"
-            f"__Here's where {get_profile['firstName']} worked:__\n\n"
-            f"{time_string}__Here's where {get_profile['firstName']} studied:__\n\n{school_string}"
-        )
-
-        colors = Colors()
-        random_color = colors.random_color()
-        final_color = discord.Colour(random_color)
-
-        embed_content = discord.Embed(
-            title=f"A look at {get_profile['firstName']} {get_profile['lastName']}'s profile.",
-            type="rich",
-            description=description,
-            color=final_color,
-        )
-
-        embed_content.set_thumbnail(
-            url=get_profile["displayPictureUrl"] + get_profile["img_100_100"]
-        )
-
-        await ctx.send(embed=embed_content)
+        self.linkedin_job_api = LinkedIn()
 
     @cog_ext.cog_slash(name="post-job", description=Descriptions.POST_JOB)
     async def _post_job(self, ctx: SlashContext, url_or_jobid: str):
