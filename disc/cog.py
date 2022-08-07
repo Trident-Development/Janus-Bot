6d913ac0933cd6a1e7dd0057de944461cb7e8e32
Allen Tran <allentran08@gmail.com>
2022-08-06 19:07:04 -0500

    view profile card works

--- disc/cog.py
+++ disc/cog.py
@@ -1,10 +1,15 @@
+from code import interact
 import discord
+import time
 from discord.ext import commands
 from discord_slash import cog_ext
 from discord_slash import SlashContext
 
 from job_boards_scrapers import LinkedIn
 from utils import Colors
+from config import LINKEDIN_ACCOUNT, LINKEDIN_PASSWORD
+
+from linkedin_api import Linkedin
 
 class Descriptions:
     POST_JOB = "Parse a job post's information beautifully in the channel"
@@ -14,6 +19,58 @@ class Descriptions:
 class Slash(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.api = Linkedin(LINKEDIN_ACCOUNT, LINKEDIN_PASSWORD)
+
+    @cog_ext.cog_slash(name="view-profile", description=Descriptions.POST_JOB)
+    async def _view_profile(self, ctx: SlashContext, profile):
+
+        await ctx.send("Hang on, grabbing details for you...", hidden=True)
+
+        get_profile = self.api.get_profile(profile)
+
+        time_string = ""
+        for job in get_profile['experience']:
+            time_period = job['timePeriod']
+            if 'endDate' in time_period:
+                time_string += (
+                    f"**🚀 {job['companyName']}** ({time_period['startDate']['month']},"
+                    f"{time_period['startDate']['year']} to {time_period['endDate']['month']}, "
+                    f"{time_period['endDate']['year']})\n*{job['title']}*\n"
+                    
+                )
+            else:
+                time_string += (
+                    f"**🚀 {job['companyName']}**\n ({time_period['startDate']['month']}, "
+                    f"{time_period['startDate']['year']} to Present)\n\n*{job['title']}*\n"
+                )
+        
+        school_string = ""
+        for education in get_profile['education']:
+                school_string += f"**🎖 {education['schoolName']}**\n\n"
+                
+
+        description = (
+            f"**{get_profile['headline']}**\n\n"
+            f"Based in {get_profile['geoLocationName']}, {get_profile['location']['basicLocation']['countryCode'].upper()}, "
+            f"{get_profile['firstName']} is currently working in the **{get_profile['industryName']}** industry.\n\n"
+            f"__Here's {get_profile['firstName']}'s experience so far:__\n\n"
+            f"{time_string}\n__Here's where {get_profile['firstName']} studied:\n\n{school_string}"
+        )
+        
+        colors = Colors()
+        random_color = colors.random_color()
+        final_color = discord.Colour(random_color)
+
+        embed_content = discord.Embed(
+            title=f"A look at {get_profile['firstName']} {get_profile['lastName']}'s profile.",
+            type="rich",
+            description=description,
+            color=final_color
+        )
+
+        embed_content.set_thumbnail(url=get_profile['displayPictureUrl'] + get_profile['img_100_100'])
+        
+        await ctx.send(embed=embed_content)
 
     @cog_ext.cog_slash(name="post-job", description=Descriptions.POST_JOB)
     async def _post_job(self, ctx: SlashContext, job):
