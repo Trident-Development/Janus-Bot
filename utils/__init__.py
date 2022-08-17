95292af627b44557b2335ed067545656611db77b
LuanTruong <truongluan303@gmail.com>
2022-08-17 14:04:39 -0700

    Setup github CI pipeline

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,13 +1,14 @@
 from random import randint
 
+
 class Colors:
     def __init__(self):
-        self.color_list = [0x36a64f, 0x288BA8, 0x746AB0, 0xE83845, 0xFFCE30, 0xE389B9]
+        self.color_list = [0x36A64F, 0x288BA8, 0x746AB0, 0xE83845, 0xFFCE30, 0xE389B9]
 
     def random_color(self):
         try:
-            number = randint(0, len(self.color_list)-1)
+            number = randint(0, len(self.color_list) - 1)
         except:
-            number = 0 
+            number = 0
         finally:
             return self.color_list[number]
