be2d151d45dc6c27d79119c484855f0246c9890f
Luan Truong <83048295+truongluan303@users.noreply.github.com>
2022-10-08 00:03:03 -0700

    Update api to call the correct new endpoint

--- job_api_wrapers/linkedin.py
+++ job_api_wrapers/linkedin.py
@@ -8,7 +8,7 @@ from .datatypes import JobInfo
 
 def get_job_info(url_or_id: Union[str, int]) -> Optional[dict]:
     response = requests.get(
-        f"https://geek-pursuit.herokuapp.com/linkedin/job-info",
+        f"https://geek-pursuit.herokuapp.com/linkedin/linkedin-job-info",
         params={"job_url_or_id": url_or_id},
     )
     if response.status_code == 400:
